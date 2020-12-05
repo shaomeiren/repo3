ffd22b290aa039cdeff78ba5548d6f0568621242
shaomeiren <shao892763781@qq.com>
2020-12-05 10:52:41 +0800

    添加springbootquick工程

---
+++ sopringboot_quick/src/main/java/com/heima/MySpringboot.java
@@ -0,0 +1,12 @@
+package com.heima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autofigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringboot {
+
+    public static void main(String[] args){
+        SpringApplication.run(MySpringboot.class);
+    }
+}
